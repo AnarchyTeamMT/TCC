45fa767e5f169b3f3f60f00e0f4204a56002adee
PrimoDude in Minetest <108569361+PrimoDude@users.noreply.github.com>
2022-07-02 11:07:17 +0300

    add script for downloading Teran Client

---
+++ TeranClient.sh
@@ -0,0 +1,9 @@
+git clone --depth 1 https://github.com/Teeranu/TeranClient
+cd TeranClient
+
+git clone --depth 1 https://github.com/minetest/minetest_game.git games/minetest_game
+
+git clone --depth 1 https://github.com/minetest/irrlicht.git lib/irrlichtmt
+
+cmake . -DRUN_IN_PLACE=TRUE
+make -j$(nproc)
