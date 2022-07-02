d783edf71b6fd16b5876330bda5fcd93abc9e3a2
AB in MT <96744017+AB-MT@users.noreply.github.com>
2022-07-02 11:21:19 +0300

    Remove repo downloading

--- TeranClient.sh
+++ TeranClient.sh
@@ -1,6 +1,3 @@
-git clone --depth 1 https://github.com/Teeranu/TeranClient
-cd TeranClient
-
 git clone --depth 1 https://github.com/minetest/minetest_game.git games/minetest_game
 
 git clone --depth 1 https://github.com/minetest/irrlicht.git lib/irrlichtmt
