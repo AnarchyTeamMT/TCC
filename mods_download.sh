15e89066688c613950d5df840c0c813035666b72
AB in MT <96744017+AB-MT@users.noreply.github.com>
2022-07-07 20:49:13 +0300

    Add cheat mods downloading

---
+++ mods_download.sh
@@ -0,0 +1,22 @@
+git clone --depth 1 https://github.com/dragonfireclient/killaura clientmods/killaura
+git clone --depth 1 https://github.com/dragonfireclient/nametags clientmods/nametags
+git clone --depth 1 https://github.com/dragonfireclient/autotool clientmods/autotool
+git clone --depth 1 https://github.com/dragonfireclient/simpletp clientmods/simpletp
+git clone --depth 1 https://github.com/dragonfireclient/karaoke clientmods/karaoke
+git clone --depth 1 https://github.com/dragonfireclient/diglib clientmods/diglib
+git clone --depth 1 https://github.com/dragonfireclient/digcustom clientmods/digcustom
+git clone --depth 1 https://github.com/dragonfireclient/autokey clientmods/ autokey
+git clone --depth 1 https://github.com/dragonfireclient/draining clientmods/draining
+git clone --depth 1 https://github.com/dragonfireclient/autoeat clientmods/autoeat
+git clone --depth 1 https://github.com/dragonfireclient/autodupe clientmods/autodupe
+git clone --depth 1 https://github.com/dragonfireclient/invutil clientmods/invutil
+git clone --depth 1 https://github.com/dragonfireclient/crystalpvp clientmods/crystalpvp
+git clone --depth 1 https://github.com/dragonfireclient/enderchest clientmods/enderchest
+git clone --depth 1 https://github.com/dragonfireclient/worldutil clientmods/worldutil
+git clone --depth 1 https://github.com/dragonfireclient/noweather clientmods/noweather
+git clone --depth 1 https://github.com/dragonfireclient/physics_override clientmods/physics_override
+git clone --depth 1 https://github.com/dragonfireclient/chateffects clientmods/chateffects
+git clone --depth 1 https://github.com/dragonfireclient/bowaimbot clientmods/bowaimbot
+git clone --depth 1 https://github.com/dragonfireclient/perlin clientmods/perlin
+git clone --depth 1 https://github.com/dragonfireclient/warp clientmods/warp
+git clone --depth 1 https://github.com/dragonfireclient/pathfinding clientmods/pathfinding
