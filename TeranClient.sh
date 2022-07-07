8b0ac60a623c3d26e7b9c76b0f9d4fd0ffa8cbbd
AB in MT <96744017+AB-MT@users.noreply.github.com>
2022-07-07 20:55:31 +0300

    Add downloading and auto start in client

--- TeranClient.sh
+++ TeranClient.sh
@@ -1,6 +1,10 @@
 git clone --depth 1 https://github.com/minetest/minetest_game.git games/minetest_game
 
+bash mods_download.sh
+
 git clone --depth 1 https://github.com/minetest/irrlicht.git lib/irrlichtmt
 
 cmake . -DRUN_IN_PLACE=TRUE
 make -j$(nproc)
+
+./bin/minetest
